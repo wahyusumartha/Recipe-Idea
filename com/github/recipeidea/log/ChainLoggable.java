875d66fa75e4981eb629bc643935e27d95bd36cd
wahyusumartha <wahyusumartha@gmail.com>
2010-11-03 01:27:20 +0700

    Core Commit

---
+++ com/github/recipeidea/log/ChainLoggable.java
@@ -0,0 +1,22 @@
+
+package com.github.recipeidea.log;
+
+public interface ChainLoggable extends Loggable {
+
+	public String getName();
+
+	public void setName(String pName);
+
+	public int getLevel();
+
+	public void setLevel(int pLevel);
+
+	public boolean getAdditive();
+
+	public void setAdditive(boolean pAdditive);
+
+	public ChainLoggable getParent();
+
+	public void setParent(ChainLoggable pParent);
+
+}

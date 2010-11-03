d7f182af14918c5c5766c50e7da2346ce0269c5d
wahyusumartha <wahyusumartha@gmail.com>
2010-11-03 12:56:45 +0700

    add httpConnection and httpClient to Connect with Server

---
+++ com/github/recipeidea/network/HttpClient.java
@@ -0,0 +1,81 @@
+package com.github.recipeidea.network;
+
+import java.io.InputStream;
+
+import javax.microedition.io.HttpConnection;
+
+import com.github.recipeidea.log.Logger;
+
+public class HttpClient {
+
+	private HttpConnectionFactory factory;
+
+	protected Logger log = Logger.getLogger(getClass());
+
+	public HttpClient(HttpConnectionFactory hFactory) {
+		factory = hFactory;
+	}
+
+	/*
+	 * Get Method Connection
+	 * 
+	 * @url Url to Server
+	 * 
+	 * @parameterField is an adding parameter to url
+	 */
+	public StringBuffer doGet(String url, String parameterField,
+			HttpConnectionFactory hFactory) throws Exception {
+		return doGet(url + parameterField, hFactory);
+	}
+
+	/*
+	 * GET Method Connection
+	 * 
+	 * @url url to Server
+	 * 
+	 * @hFactory HttpConnection
+	 */
+	public StringBuffer doGet(String url, HttpConnectionFactory hFactory)
+			throws Exception {
+		HttpConnection connection = null;
+		StringBuffer stringBuffer = new StringBuffer();
+
+		try {
+
+			if (url == null || url.equals("") || url.length() == 0) {
+				log.debug(getClass().getName() + " : Url Not Initialized");
+				return null;
+			}
+
+			connection = hFactory.getHttpConnection(url);
+
+			switch (connection.getResponseCode()) {
+			case HttpConnection.HTTP_OK: {
+				InputStream inputStream = connection.openInputStream();
+				int c;
+				while ((c = inputStream.read()) != -1) {
+					stringBuffer.append(c);
+				}
+				inputStream.close();
+				break;
+			}
+			case HttpConnection.HTTP_TEMP_REDIRECT:
+			case HttpConnection.HTTP_MOVED_TEMP:
+			case HttpConnection.HTTP_MOVED_PERM: {
+				url = connection.getHeaderField("Location");
+				stringBuffer = doGet(url, hFactory);
+				break;
+			}
+			default:
+				break;
+			}
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (connection != null)
+				connection.close();
+		}
+		return stringBuffer;
+	}
+}
---
+++ com/github/recipeidea/network/HttpConnectionFactoryException.java
@@ -0,0 +1,13 @@
+package com.github.recipeidea.network;
+
+public class HttpConnectionFactoryException extends Exception {
+
+	public HttpConnectionFactoryException() {
+		super();
+	}
+
+	public HttpConnectionFactoryException(String s) {
+		super(s);
+	}
+
+}

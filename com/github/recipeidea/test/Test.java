09628fb36a12aef6657160af722126771e4d7ec2
wahyusumartha <wahyusumartha@gmail.com>
2010-11-04 02:11:21 +0700

    Test.java : Update TestScreen and make it compatible with core's pattern

--- com/github/recipeidea/test/Test.java
+++ com/github/recipeidea/test/Test.java
@@ -9,38 +9,44 @@ import com.github.recipeidea.json.JSONException;
 import com.github.recipeidea.log.Logger;
 import com.github.recipeidea.network.HttpClient;
 import com.github.recipeidea.network.HttpConnectionFactory;
+import com.github.recipeidea.ui.RecipeIdeaScreen;
 
 import net.rim.device.api.ui.component.Dialog;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
 
-public class Test extends MainScreen {
+public class Test extends RecipeIdeaScreen {
 	private LabelField label;
 
-	private HttpConnectionFactory factory;
-	private ServiceClient services;
-	private RecipeIdeaAppSetting setting;
+	// private HttpConnectionFactory factory;
+	// private ServiceClient services;
+	// private RecipeIdeaAppSetting setting;
 	// private HttpClient httpClient;
+
+	public static final String ACTION_ENTER = "testScreen";
+	public static final String ACTION_SUCCESS = "success";
+	public static final String ACTION_ERROR = "error";
 
 	private Recipe[] recipe;
 	protected Logger log = Logger.getLogger(getClass());
 
-	public Test() throws Exception {
-		factory = new HttpConnectionFactory();
-		setting = new RecipeIdeaAppSetting("http://www.recipepuppy.com/api/");
-		services = new ServiceClient(setting, factory);
+	public Test(ServiceClient serviceClient) throws Exception {
+		super(serviceClient);
+		// factory = new HttpConnectionFactory();
+		// setting = new
+		// RecipeIdeaAppSetting("http://www.recipepuppy.com/api/");
+		// services = new ServiceClient(setting, factory);
 		label = new LabelField();
 
-		services.setParameter("tomato");
-		String cook = null;
-		JSONArray array = services.getRecipeAccess().getRecipes();
-		// log.debug(recipe[0].toString());
+		// services.setParameter("tomato");
+		serviceClient.setParameter("ginger");
+
 		StringBuffer buffer = new StringBuffer();
-		for (int i = 0; i < array.length(); i++) {
-			buffer.append(array.getJSONObject(i).get("title")).append("\n");
+		recipe = serviceClient.getRecipeAccess().getRecipe();
+		for (int i = 0; i < recipe.length; i++) {
+			buffer.append(recipe[i].getTitle()).append("\n");
 		}
 		label.setText(buffer.toString());
-		// log.debug(response.toString());
 
 		add(label);
 	}

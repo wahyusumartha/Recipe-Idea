fce148ea00adfcc3062e8f38591c9cc2929ae716
wahyusumartha <wahyusumartha@gmail.com>
2010-11-04 16:44:55 +0700

    Finalized RecipesScreen for v1.0.0 alpha

--- com/github/recipeidea/ui/RecipesScreen.java
+++ com/github/recipeidea/ui/RecipesScreen.java
@@ -6,11 +6,13 @@ import java.util.Vector;
 
 import com.github.recipeidea.Recipe;
 import com.github.recipeidea.ServiceClient;
-import com.github.recipeidea.ServiceClientException;
 import com.github.recipeidea.log.Logger;
+import com.github.recipeidea.ui.component.BackgroundManager;
+import com.github.recipeidea.ui.component.HeaderField;
 
 import net.rim.device.api.system.Bitmap;
 import net.rim.device.api.system.Display;
+import net.rim.device.api.ui.Color;
 import net.rim.device.api.ui.DrawStyle;
 import net.rim.device.api.ui.Font;
 import net.rim.device.api.ui.FontFamily;
@@ -19,6 +21,7 @@ import net.rim.device.api.ui.Ui;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.ListField;
 import net.rim.device.api.ui.component.ListFieldCallback;
+import net.rim.device.api.ui.component.SeparatorField;
 
 final class RecipesScreen extends RecipeIdeaScreen {
 
@@ -26,21 +29,29 @@ final class RecipesScreen extends RecipeIdeaScreen {
 	static final String ACTION_SUCCESS = "success";
 	static final String ACTION_ERROR = "error";
 
-	private static final String LABEL_TITLE = "The Recipes";
+	private static final String BANNER_TITLE = "The Recipes For You";
 
 	private ListField listField;
-	private StreamListCallBack streamListCallBack;
+	private StreamListCallBack streamListCallBack = new StreamListCallBack();
+
+	private HeaderField banner;
+	private BackgroundManager backgroundManager;
 
 	protected Logger log = Logger.getLogger(getClass());
 
 	public RecipesScreen(ServiceClient serviceClient) {
 		super(serviceClient);
-		setTitle(new LabelField(LABEL_TITLE, LabelField.ELLIPSIS
-				| LabelField.USE_ALL_WIDTH));
-
+		banner = new HeaderField(BANNER_TITLE);
+		// backgroundManager = new BackgroundManager();
 		listField = new ListField();
+		listField.setRowHeight(50);
 		listField.setCallback(streamListCallBack);
+		add(banner);
+		add(new SeparatorField());
 		add(listField);
+		// backgroundManager.add(banner);
+		// backgroundManager.add(listField);
+		// add(backgroundManager);
 	}
 
 	public void loadList() {
@@ -49,7 +60,9 @@ final class RecipesScreen extends RecipeIdeaScreen {
 		}
 
 		try {
+			// serviceClient.setParameter("Tomato");
 			Recipe[] recipes = serviceClient.getRecipeAccess().getRecipe();
+			log.debug(recipes[0].getTitle());
 			streamListCallBack.clear();
 
 			for (int i = 0; i < recipes.length; i++) {
@@ -58,8 +71,9 @@ final class RecipesScreen extends RecipeIdeaScreen {
 			}
 			// Load Thumbnail
 			streamListCallBack.loadBitmaps();
-		} catch (ServiceClientException e) {
+		} catch (Exception e) {
 			log.error(e.getMessage());
+			fireAction(ACTION_ERROR, e.getMessage());
 		}
 
 	}
@@ -87,17 +101,45 @@ final class RecipesScreen extends RecipeIdeaScreen {
 		public void drawListRow(ListField listField, Graphics graphics,
 				int index, int y, int width) {
 			if (index < recipes.size()) {
-				int height = listField.getHeight();
+				int height = listField.getRowHeight();
+				// log.info("List Height : " + height);
+
 				Recipe recipe = (Recipe) recipes.elementAt(index);
 				Bitmap bitmap = getBitmap(recipe.getTitle());
+				/*
+				 * Draw List Cell every Row
+				 */
+				int[] upperX_PTS = new int[] { 0, 0, width, width };
+				int[] upperY_PTS = new int[] { 0, y + (height / 2),
+						y + (height / 2), 0 };
 
+				int[] lowerX_PTS = new int[] { 0, 0, width, width };
+				int[] lowerY_PTS = new int[] { y + (height / 2), y + height,
+						y + height, y + (height / 2) };
+
+				int[] upperDrawColors = new int[] { 0xa0acc2, 0x7788a4,
+						0x7788a4, 0xa0acc2 };
+				int[] lowerDrawColors = new int[] { 0x70819d, 0x606e88,
+						0x606e88, 0x70819d };
+				graphics.drawShadedFilledPath(upperX_PTS, upperY_PTS, null,
+						upperDrawColors, null);
+				graphics.drawShadedFilledPath(lowerX_PTS, lowerY_PTS, null,
+						lowerDrawColors, null);
+				/*
+				 * End of Draw List Cell
+				 */
 				if (bitmap != null) {
+					// log.info("Bitmap Retrieved : "
+					// + getBitmap(recipe.getTitle()) + " Height : "
+					// + getBitmap(recipe.getTitle()).getHeight());
+
 					graphics.drawBitmap(0,
 							y
 									+ ((height - Math.min(bitmap.getHeight(),
 											height)) / 2), 50, height, bitmap,
 							0, 0);
 				}
+
 				FontFamily fontFamily = null;
 				Font font = null;
 				try {
@@ -105,14 +147,17 @@ final class RecipesScreen extends RecipeIdeaScreen {
 				} catch (ClassNotFoundException e) {
 					font = Font.getDefault();
 				}
-				font = fontFamily.getFont(Font.PLAIN, 6, Ui.UNITS_pt);
-
+				font = fontFamily.getFont(Font.BOLD, 6, Ui.UNITS_pt);
 				graphics.setFont(font);
 				// draw title
-				graphics.drawText(recipe.getTitle(), 52, y, 0, width - 52);
+				graphics.drawText(recipe.getTitle(), 52, y, DrawStyle.ELLIPSIS,
+						width - 52);
+				font = fontFamily.getFont(Font.PLAIN, 5, Ui.UNITS_pt);
+				graphics.setFont(font);
 				// draw ingredients
 				graphics.drawText("Ingredients : " + recipe.getIngredients(),
 						52, y + (height / 2), DrawStyle.ELLIPSIS, width - 52);
+
 				graphics.drawLine(0, y + height - 1, width, y + height - 1);
 			}
 		}
